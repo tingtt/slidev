cf9eacdda63ddf24a6d94a3d48eea425278e4317
tingtt <ting.taku@gmail.com>
2022-07-25 15:18:22 +0900

    [add] progress bar

---
+++ components/Progress.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="absolute z-50 h-[0.5rem] bottom-0 left-0 right-0 flex justify-center">
+        <div class="w-full relative h-full bg-cyan-500 overflow-hidden">
+            <span class="absolute h-full bg-gray-500 transition-all duration-300" :style="style_current"></span>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: "Progress",
+    props: [ 'current', 'total' ],
+    computed: {
+      style_current() {
+        return {
+          width: `${100 / this.total}%`,
+          left: `${100 / this.total * (this.current - 1)}%`,
+        }
+      }
+    }
+  };
+</script>
---
+++ global-bottom.vue
@@ -0,0 +1,21 @@
+<template>
+  <Progress
+    v-if="$slidev.nav.currentPage !== 1 && $slidev.nav.currentLayout!== 'end'"
+    :current="$slidev.nav.currentPage"
+    :total="$slidev.nav.total"
+  />
+</template>
+
+<script lang="ts">
+  export default {
+    name: "Progress",
+    computed: {
+      style_current() {
+        return {
+          width: `${100 / this.total}%`,
+          left: `${100 / this.total * (this.current - 1)}%`,
+        }
+      }
+    }
+  };
+</script>
